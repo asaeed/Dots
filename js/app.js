a78961c8bef28ca8a0f4c435fe94fd5d20e4bf5b
Ahmad Saeed <asaeed@gmail.com>
2016-07-21 15:33:09 -0400

    add animation behavior via composition

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var gridGap = 100, gridW = 100, gridH = 40;
+var gridGap = 100, gridW = 20, gridH = 20;
 
 var container, stats;
 var camera, scene, renderer, controls;
@@ -6,7 +6,6 @@ var camera, scene, renderer, controls;
 var dotController;
 
 var count = 0;
-
 var mouseX = 0, mouseY = 0;
 
 var ww = window.innerWidth;
@@ -20,22 +19,19 @@ function init() {
     document.body.appendChild(container);
 
     camera = new THREE.PerspectiveCamera(75, ww/wh, 1, 10000);
-    // camera.position.z = 0;
-    // camera.position.x = ww/2;
-    // camera.position.y = wh/2;
     
-    // controls = new THREE.OrbitControls(camera);
-    // controls.enableDamping = true;
-    // controls.dampingFactor = 0.25;
-    // controls.enableZoom = true;
+    controls = new THREE.OrbitControls(camera);
+    controls.enableDamping = true;
+    controls.dampingFactor = 0.25;
+    controls.enableZoom = true;
 
     scene = new THREE.Scene();
 
-    camera.position.set(ww/2, 1000, wh/2);
+    camera.position.set(0, 1000, 0);
     camera.up = new THREE.Vector3(0, 1, 0);
     camera.lookAt(scene.position);
 
-    dotController = new DotController(scene, gridW, gridH, gridGap)
+    dotController = new DotController(scene, gridW, gridH, gridGap, wavesAnimator)
     dotController.setup();
 
     renderer = new THREE.CanvasRenderer();
@@ -43,13 +39,12 @@ function init() {
     renderer.setSize(ww, wh);
     container.appendChild(renderer.domElement);
 
-    renderer.render(scene, camera);
-
     stats = new Stats();
     stats.showPanel(0);
     document.body.appendChild(stats.dom);
 
     window.addEventListener('resize', onWindowResize, false);
+    window.addEventListener('mousemove', onMouseMove, false);
 }
 
 function onWindowResize() {
@@ -60,6 +55,18 @@ function onWindowResize() {
     camera.updateProjectionMatrix();
 
     renderer.setSize(ww, wh);
+}
+
+function onMouseMove(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+    // vector.set((e.clientX / ww) * 2 - 1, - (e.clientY / wh ) * 2 + 1);
+    // raycaster.setFromCamera(vector,camera);
+
+    // // check for intersects with cubes in grid
+    // intersects = raycaster.intersectObjects(elements.children);
+    // if (intersects.length > 0) {
+    //     var cube = intersects[0].object;
 }
 
 function animate() {
--- js/dotController.js
+++ js/dotController.js
@@ -2,15 +2,35 @@
 const barker = (state) => ({
     bark: () => console.log('Woof, I am ' + state.name)
 })
-const driver = (state) => ({
-    drive: () => state.position = state.position + state.speed
-})
+
+const wavesAnimator = {
+    update: (controller) => {
+        var c = controller;
+        var i = 0;
+        for ( var ix = 0; ix < c.grid.w; ix++ ) {
+            for ( var iy = 0; iy < c.grid.h; iy++ ) {
+                var dot = c.dots[i++];
+                dot.position.y = (Math.sin((ix + count) * 0.3) * 50) + (Math.sin((iy + count) * 0.5) * 50);
+                dot.scale.x = dot.scale.y = (Math.sin((ix + count) * 0.3) + 1) * 4 + (Math.sin((iy + count) * 0.5) + 1) * 4;
+            }
+        }
+        count += 0.1;
+    }
+}
+
+const mouseAnimator = {
+    update: (controller) => {
+        //console.log('mouse position: ' + mouseX + ', ' + mouseY);
+        
+    }
+}
 
 class DotController {
-    constructor(scene, gridW, gridH, gridGap) {
+    constructor(scene, gridW, gridH, gridGap, animator) {
         this.scene = scene
         this.grid = { w: gridW, h: gridH, gap: gridGap}
         this.dots = new Array();
+        this.animator = animator;
     }
 
     setup() {
@@ -24,8 +44,8 @@ class DotController {
         });
 
         var i = 0;
-        for ( var ix = 0; ix < this.grid.w; ix ++ ) {
-            for ( var iy = 0; iy < this.grid.h; iy ++ ) {
+        for ( var ix = 0; ix < this.grid.w; ix++ ) {
+            for ( var iy = 0; iy < this.grid.h; iy++ ) {
                 var dot = this.dots[i++] = new THREE.Sprite(material);
                 dot.position.x = ix * this.grid.gap - ((this.grid.w * this.grid.gap) / 2);
                 dot.position.z = iy * this.grid.gap - ((this.grid.h * this.grid.gap) / 2);
@@ -35,15 +55,6 @@ class DotController {
     }
 
     update() {
-        var i = 0;
-        for ( var ix = 0; ix < this.grid.w; ix ++ ) {
-            for ( var iy = 0; iy < this.grid.h; iy ++ ) {
-
-                var dot = this.dots[ i++ ];
-                dot.position.y = (Math.sin((ix + count) * 0.3) * 50) + (Math.sin((iy + count) * 0.5) * 50);
-                dot.scale.x = dot.scale.y = (Math.sin((ix + count) * 0.3) + 1) * 4 + (Math.sin((iy + count) * 0.5) + 1) * 4;
-            }
-        }
-        count += 0.1;
+        this.animator.update(this);
     }
 }
